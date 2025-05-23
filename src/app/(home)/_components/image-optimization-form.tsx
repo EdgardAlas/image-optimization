239dcecbbdc218f8e8e787ec51903a419d1df9ac
Edgard Alas <edgardalasu@gmail.com>
2025-05-23 11:17:09 -0600

    feat: create ImageOptimizationForm component and add image formats data for optimization

---
+++ src/app/(home)/_components/image-optimization-form.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { OptimizationSettings } from '@/app/(home)/_components/optimization-settings';
+import { Card, CardContent } from '@/components/ui/card';
+
+export const ImageOptimizationForm = () => {
+	return (
+		<section className='grid w-full flex-1 gap-4 lg:min-h-[725px] lg:grid-cols-12 lg:gap-8'>
+			<section className='col-span-12 grid gap-4 lg:col-span-4 lg:gap-8'>
+				<section>
+					<Card className='h-full w-full'>
+						<CardContent>Image Uploader</CardContent>
+					</Card>
+				</section>
+				<section>
+					<OptimizationSettings />
+				</section>
+			</section>
+			<section className='col-span-12 max-h-[725px] overflow-y-auto lg:col-span-8'>
+				<Card className='h-full w-full'>
+					<CardContent>Summary of optimized images</CardContent>
+				</Card>
+			</section>
+		</section>
+	);
+};
---
+++ src/data/image-formats.tsx
@@ -0,0 +1,23 @@
+export const imageFormats = [
+	{
+		label: 'JPG',
+		value: 'jpeg',
+	},
+	{
+		label: 'PNG',
+		value: 'png',
+	},
+	{
+		label: 'AVIF',
+		value: 'avif',
+	},
+	{
+		label: 'SVG',
+		value: 'svg',
+	},
+	{
+		label: 'WebP',
+		value: 'webp',
+	},
+];
+
