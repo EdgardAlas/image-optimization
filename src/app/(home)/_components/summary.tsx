6b643956bbae90dbf506e55ec460f853f51b51d0
Edgard Alas <edgardalasu@gmail.com>
2025-05-24 21:11:48 -0600

    refactor: extract Loader component for improved readability

--- src/app/(home)/_components/summary.tsx
+++ src/app/(home)/_components/summary.tsx
@@ -36,38 +36,7 @@ export const Summary = () => {
 	};
 
 	if (form.formState.isSubmitting) {
-		return (
-			<Card className='col-span-12 max-h-[730px] overflow-y-auto lg:col-span-8'>
-				<CardContent className='h-full'>
-					<div className='flex h-full items-center justify-center'>
-						<svg
-							className='animate-spin'
-							width='48'
-							height='48'
-							viewBox='0 0 48 48'
-							fill='none'
-							aria-label='Loading'
-							xmlns='http://www.w3.org/2000/svg'
-						>
-							<circle
-								className='stroke-foreground opacity-25'
-								cx='24'
-								cy='24'
-								r='20'
-								strokeWidth='6'
-								fill='none'
-							/>
-							<path
-								className='stroke-foreground opacity-75'
-								d='M44 24c0-11.046-8.954-20-20-20'
-								strokeWidth='6'
-								strokeLinecap='round'
-							/>
-						</svg>
-					</div>
-				</CardContent>
-			</Card>
-		);
+		return <Loader />;
 	}
 
 	return (
@@ -196,3 +165,36 @@ const UploadedImageCard = ({ image }: UploadedImageCardProps) => {
 		</Card>
 	);
 };
+
+const Loader = () => (
+	<Card className='col-span-12 max-h-[730px] overflow-y-auto lg:col-span-8'>
+		<CardContent className='h-full'>
+			<div className='flex h-full items-center justify-center'>
+				<svg
+					className='animate-spin'
+					width='48'
+					height='48'
+					viewBox='0 0 48 48'
+					fill='none'
+					aria-label='Loading'
+					xmlns='http://www.w3.org/2000/svg'
+				>
+					<circle
+						className='stroke-foreground opacity-25'
+						cx='24'
+						cy='24'
+						r='20'
+						strokeWidth='6'
+						fill='none'
+					/>
+					<path
+						className='stroke-foreground opacity-75'
+						d='M44 24c0-11.046-8.954-20-20-20'
+						strokeWidth='6'
+						strokeLinecap='round'
+					/>
+				</svg>
+			</div>
+		</CardContent>
+	</Card>
+);
